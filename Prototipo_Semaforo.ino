b331b0d9047e6a63891ab52176c52c1911d9bc7e
Víctor Nieves <vnievess@gmail.com>
2017-04-27 20:33:56 +0200

    Parpadeo de LED
    "Semáforo"
    Inicializo los pins digitales 11,10,9 como salidas

---
+++ Prototipo_Semaforo.ino
@@ -0,0 +1,20 @@
+void setup() {
+  // put your setup code here, to run once:
+  //Inicializo los pins digitales 11,10,9 como salidas
+  pinMode(11,OUTPUT);
+  pinMode(10,OUTPUT);
+  pinMode(9,OUTPUT);
+  }
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(11,HIGH); //Encendemos el pin 11 (verde)
+  delay(2000); //delay de 2s
+  digitalWrite(11,LOW); //Apagamos el pin 11 (verde)
+  digitalWrite(10,HIGH); //Encendemos el pin 10 (amarillo)
+  delay(1000); //delay de 1s
+  digitalWrite(10,LOW); //Apagamos el pin 10 (amarillo)
+  digitalWrite(9,HIGH); //Encendemos el pin 9 (rojo)
+  delay(2000); //delay de 2s
+  digitalWrite(9,LOW); //Apagamos el pin 9 (rojo)
+}
